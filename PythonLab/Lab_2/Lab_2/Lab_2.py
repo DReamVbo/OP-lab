722c33ed545208693f7a843061895d32489d0aee
Vadim <90787421+DReamVbo@users.noreply.github.com>
2021-10-13 23:18:25 +0300

    Add files via upload

---
+++ PythonLab/Lab_2/Lab_2/Lab_2.py
@@ -0,0 +1,18 @@
+print("Enter y")
+y = float( input());
+print("Enter x");
+x = float( input());
+
+if ((y>=-(x-1) and y<=0 and x>1) or (x<=0 and y>1 and x>=-(y-1))):
+  print("The point belongs to the area")  
+else:
+   print("Does not belong to the area") 
+    
+    
+    
+    
+
+    
+    
+
+
